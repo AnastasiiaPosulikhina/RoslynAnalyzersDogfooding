1a6181f503840b35f6434afc647400d72797154f
Anastasiia.Posulikhi <anastasiia.posulikhina@jetbrains.com>
2023-02-17 07:10:46 +0200

    Add SupportRegexGenerator class

---
+++ NET7SourceGeneratorUser/.NET 7 Source Generators/Support RegexGenerator66.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NET7SourceGeneratorUser.NET_7_Source_Generators;
+
+public partial class SupportRegexGenerator
+{
+    [GeneratedRegex(
+        @"^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?$",
+        RegexOptions.CultureInvariant,
+        500)]
+    private static partial Regex MatchIfValidUrl();
+
+    public static bool IsValidUrl(string url)
+    {
+        var regex = new Regex(@"\G(\d{1,3})((?=(?:\d{3})+$))", RegexOptions.IgnoreCase);
+        
+        return MatchIfValidUrl().IsMatch(url);
+    }
+}
